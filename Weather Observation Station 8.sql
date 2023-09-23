579b00406c087eae8ad782dc12d903ca5d919e84
Yomna Taher <60919844+yomnataher@users.noreply.github.com>
2023-09-23 14:23:54 +0300

    Create Weather Observation Station 8.sql

---
+++ Weather Observation Station 8.sql
@@ -0,0 +1,3 @@
+Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+****************************************************************************************
+SELECT DISTINCT CITY FROM STATION WHERE CITY LIKE  '[a,e,i,o,u]%' AND CITY LIKE '%[a,e,i,o,u]' ;
